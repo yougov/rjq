fa38be8fcd3ab486f3949b5f73e6c73a53928726
Lorenzo Bolla <lorenzo.bolla@yougov.com>
2018-03-05 12:13:06 +0000

    Rename aliases for consistency

--- examples/worker.rs
+++ examples/worker.rs
@@ -9,12 +9,12 @@ use rjq::Queue;
 mod errors {
     extern crate redis;
     extern crate serde_json;
-    use rjq::errors as rjqErrors;
+    use rjq::errors::Error as RjqError;
     error_chain!{
         foreign_links {
             Redis(redis::RedisError);
             Serde(serde_json::Error);
-            Rjq(rjqErrors::Error);
+            Rjq(RjqError);
         }
     }
 }
